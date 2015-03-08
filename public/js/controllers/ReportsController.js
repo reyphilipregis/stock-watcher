1bc398cb862ecaa3f55ad29a09ef5f1c20ff8a03
Rey Philip Regis <rey.regis@globalzeal.net>
2015-03-08 12:52:26 +0800

    added top to gainers/losers in the reports page (partial no css)

--- public/js/controllers/ReportsController.js
+++ public/js/controllers/ReportsController.js
@@ -1,5 +1,29 @@
-StockWatcherApp.controller( 'ReportsController', function( $scope ) {
+'use strict';
 
-	$scope.message = 'Welcome Investors and Traders! - Reports';
+StockWatcherApp.controller( 'ReportsController', [ '$scope', '$q', 'StockService', '$timeout', function( $scope, $q, StockService, $timeout ) {
+	var self  = this;
 
-} );
+	$scope.message        = 'Welcome Investors and Traders! - Reports';
+	self.secondsToRefresh = 0;
+	self.secondsMax       = 3600000; // 1 hour
+
+	// get all the stocks from the PSEi web API
+	var stocksUpdator = function () {
+
+		$timeout( function() {
+
+			StockService.getAllStocks().then( function ( result ) {
+				$scope.resultObj = result;
+			} );
+
+			stocksUpdator();
+
+			self.secondsToRefresh = self.secondsMax;
+
+		}, self.secondsToRefresh );
+
+	};
+
+	stocksUpdator();
+
+} ] );
