372c8de6c745c8ecc54cee5d6d758630c47f4ed7
Carter Binarao <carter.binarao@globalzeal.net>
2015-03-15 17:06:25 +0800

    update about us page and css

--- public/js/controllers/AboutUsController.js
+++ public/js/controllers/AboutUsController.js
@@ -8,7 +8,7 @@ StockWatcherApp.controller( 'AboutUsController', function( $scope ) {
 		{
 			'name'        : 'Rey Philip Regis',
 			'picture'     : 'philip.jpg',
-			'title'       : 'Executive Senior Developer',
+			'title'       : 'Senior Software Engineer',
 			'description' : 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis' +
 							'praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias' +
 							'excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui' +
@@ -20,19 +20,15 @@ StockWatcherApp.controller( 'AboutUsController', function( $scope ) {
 		{
 			'name'        : 'Carter Binarao',
 			'picture'     : 'carter.jpg',
-			'title'       : 'Executive Senior Developer',
-			'description' : 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis' +
-							'praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias' +
-							'excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui' +
-							'officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem' +
-							'rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est' +
-							'eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus,' +
-							'omnis voluptas assumenda est, omnis dolor repellendus.'
+			'title'       : 'Software Engineer',
+			'description' : 'An enthusiastic and high-achieving individual who has completed a Bachelor Degree' +
+							' major in Computer Science. Background stacks in ColdFusion, PHP, Javascript, Backbone' +
+							', Marionette, C#, ASP.net, MySQL and MSSQL.'
 		},
 		{
 			'name'        : 'Axon Moulic',
 			'picture'     : 'axon.jpg',
-			'title'       : 'Executive Senior QA',
+			'title'       : 'Senior Quality Assurance Engineer',
 			'description' : 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis' +
 							'praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias' +
 							'excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui' +
